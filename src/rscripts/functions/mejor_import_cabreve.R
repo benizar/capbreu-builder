8322591e4edafddb35bf60a6f55a4e4fbe5dd3a1
benizar <benizar@gmail.com>
2017-09-14 15:47:15 +0200

    Disambiguating yaml

--- src/rscripts/functions/mejor_import_cabreve.R
+++ src/rscripts/functions/mejor_import_cabreve.R
@@ -1,45 +1,46 @@
 library(yaml)
 library(reshape2)
 
-yaml<-yaml.load_file("src/data/capbreu_load_tests_flat.yml")
+yaml<-yaml.load_file("src/data/capbreu_latest.yml")
 
-# Metadata needed for operations
-metadata<-yaml
-metadata$Landholders<-NULL
+# Project data
+keeps <- c("Structure","Title","Description")
+proj_data <- yaml[keeps]
 
-# Schema Data
-data<-yaml$Landholders
+# Context data
+keeps <- c("Landmetrics","Aggregations")
+context_data <- yaml[keeps]
+
+# Schema Data (Schema)
+schema_data<-yaml$Landholders
 
 # Vertical shape
-data<-melt(data, id=c(Landholders))
-metadata<-melt(metadata)
+proj_data<-melt(proj_data)
+context_data<-melt(context_data)
+schema_data<-melt(schema_data, id=c(Landholders))
 
 
 # Move the landholder name from values to its own column 
-data<-merge(data,data[data$L2=="Landholder",c("value","L1")],by = "L1")
+schema_data<-merge(schema_data,schema_data[schema_data$L2=="Landholder",c("value","L1")],by = "L1")
 
 # Remove NAs
-data<-merged[complete.cases(data), ]
+schema_data<-schema_data[complete.cases(schema_data), ]
 
 # Build plot id/name
-data$L2<-paste("plot",data$L1,data$L3,sep="-")
+schema_data$L2<-paste("plot",schema_data$L1,schema_data$L3,sep="-")
 
 # So L3, the plot id is no longer needed
-data$L3 <- NULL
+schema_data$L3 <- NULL
 
 # Replace Ls by level names
-l1_name<-metadata[which(metadata$L2=="Level_1"), "value"]
-l2_name<-metadata[which(metadata$L2=="Level_2"), "value"]
+l1_name<-context_data[which(context_data$L2=="Level_1"), "value"]
+l2_name<-context_data[which(context_data$L2=="Level_2"), "value"]
 
-data[which(data$L5=="Level_2"), "L5"] <- l2_name
-data[which(data$L5=="Level_1"), "L5"] <- l1_name
-
-
-
-
+schema_data[which(schema_data$L5=="Level_2"), "L5"] <- l2_name
+schema_data[which(schema_data$L5=="Level_1"), "L5"] <- l1_name
 
 # Rename for clarity
-names(data) <- c("id_landholder","value","var","var_category","plot", "landholder")
+names(schema_data) <- c("id_landholder","value","var","var_category","plot", "landholder")
 
 
-neighbours<-unique(data[which(data$var=="Neighbours"), "value"])
+neighbours<-unique(schema_data[which(schema_data$var=="Neighbours"), "value"])
