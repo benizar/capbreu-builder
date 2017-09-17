7be9bdcbc44fc99e1f8e34e74b78e74cb3b8fb86
benizar <benizar@gmail.com>
2017-09-17 04:53:38 +0200

    rebuilding lists

--- src/rscripts/functions/mejor_import_cabreve.R
+++ src/rscripts/functions/mejor_import_cabreve.R
@@ -1,7 +1,7 @@
 library(yaml)
 library(reshape2)
 
-yaml<-yaml.load_file("git/capbreu-builder/src/data/capbreu_latest.yml")
+yaml<-yaml.load_file("git/capbreu-builder/src/data/capbreu_latest_fullname.yml")
 
 # Project data
 keeps <- c("Structure","Title","Description")
@@ -17,32 +17,42 @@ context_data<-melt(context_data)
 schema_data<-yaml$Landholders
 schema_data<-melt(schema_data)
 
-lh_nodes<-schema_data[schema_data$L3 %in% c("Name","LastName","LastName2"),c("L3","value","L2")]
-lh_nodes<-dcast(lh_nodes, L2~L3) # Paste labels ommiting NAs
-
-agg1_nodes<-schema_data[schema_data$L6 %in% c("Level_1"),c("L6","value","L2")]
-agg1_nodes<-dcast(agg_nodes, value+L2~L6) # Paste labels ommiting NAs
-
-#################
-
 # Move the landholder name from values to its own column 
-schema_data<-merge(schema_data,schema_data[schema_data$L2=="Landholder",c("value","L1")],by = "L1")
+schema_data<-merge(schema_data,schema_data[schema_data$L2=="Name",c("value","L1")],by = "L1")
 
 # Remove NAs
 schema_data<-schema_data[complete.cases(schema_data), ]
 
+# List Neighbours, try to assign an neighbour_id
+unique(schema_data[which(apply(schema_data[, -1], MARGIN = 1, function(x) any(x == "Neighbours"))), "value.x"])
+
+
+
+
 # Build plot id/name
 schema_data$L2<-paste("plot",schema_data$L1,schema_data$L3,sep="-")
-
-# So L3, the plot id is no longer needed
 schema_data$L3 <- NULL
 
-# Replace Ls by level names
+# Rename for clarity
+names(schema_data) <- c("id_landholder","value","var","var_category","id_plot", "landholder")
+
+
+# Replace Ls by level names LABELS
 l1_name<-context_data[which(context_data$L2=="Level_1"), "value"]
 l2_name<-context_data[which(context_data$L2=="Level_2"), "value"]
 
-schema_data[which(schema_data$L5=="Level_2"), "L5"] <- l2_name
-schema_data[which(schema_data$L5=="Level_1"), "L5"] <- l1_name
+#schema_data[which(schema_data$L6=="Level_2"), "L6"] <- l2_name
+#schema_data[which(schema_data$L6=="Level_1"), "L6"] <- l1_name
+
+# Filter by any (x==People)
+#schema_data[which(apply(schema_data[, -1], MARGIN = 1, function(x) any(x == "Level_1"))), ]
+agg1<-unique(schema_data[which(apply(schema_data[, -1], MARGIN = 1, function(x) any(x == "Level_1"))), "value"])
+agg2<-unique(schema_data[which(apply(schema_data[, -1], MARGIN = 1, function(x) any(x == "Level_2"))), "value"])
+
+natural<-unique(schema_data[which(apply(schema_data[, -1], MARGIN = 1, function(x) any(x == "Natural"))), "value"])
+anthropic<-unique(schema_data[which(apply(schema_data[, -1], MARGIN = 1, function(x) any(x == "Anthropic"))), "value"])
+administrative<-unique(schema_data[which(apply(schema_data[, -1], MARGIN = 1, function(x) any(x == "Administrative"))), "value"])
+
 
 # Rename for clarity
 names(schema_data) <- c("id_landholder","value","var","var_category","plot", "landholder")
