b8105d9de5718247991cf2e7333e486951839050
benizar <benizar@gmail.com>
2022-11-25 19:37:01 +0100

    revised non root permissions

--- mk/2-docker-flags.mk
+++ mk/2-docker-flags.mk
@@ -11,6 +11,7 @@ DOCKER_RUN          = $(DOCKER) run
 DOCKER_RUN_OPTIONS  = --rm
 DOCKER_RUN_WORKDIR  = --workdir /source
 DOCKER_VOLUME       = --volume $(PWD):/source
+DOCKER_USER         = -u $$(id -u):$$(id -g)
 
 # R
 RSCRIPT             = --entrypoint Rscript
@@ -18,5 +19,6 @@ RUN_RSCRIPT = $(DOCKER_RUN) $(DOCKER_RUN_OPTIONS) \
 	$(DOCKER_RUN_WORKDIR) \
 	$(RSCRIPT) \
 	$(DOCKER_VOLUME) \
+	$(DOCKER_USER) \
 	$(R_IMAGE) \
 	$(RSCRIPT_OPTIONS)
